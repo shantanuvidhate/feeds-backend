654b4d5f895ce95402fe13772bf52d87e7484b90
shantanuvidhate <shantanuvidhate00@gmail.com>
2025-01-16 01:23:44 +0530

    load envoirnment variable

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,21 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/shantanuvidhate/feeds-backend/internal/env"
+)
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR", ":8080"),
 	}
 
 	app := &application{
