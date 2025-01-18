f54e69c0ddafd90f17be0beac32cf6f826b779b1
shantanuvidhate <shantanuvidhate00@gmail.com>
2025-01-18 18:24:04 +0530

    getById fuction signature

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -3,11 +3,17 @@ package store
 import (
 	"context"
 	"database/sql"
+	"errors"
+)
+
+var (
+	ErrRecordNotFound = errors.New("record not found")
 )
 
 type Storage struct {
 	Post interface {
 		Create(context.Context, *Post) error
+		GetById(context.Context, int64) (*Post, error)
 	}
 	User interface {
 		Create(context.Context, *User) error
