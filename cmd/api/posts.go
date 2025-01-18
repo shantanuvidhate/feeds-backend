36bd47718c4cc2b7a799b468b742fc191172ed4e
shantanuvidhate <shantanuvidhate00@gmail.com>
2025-01-18 18:25:18 +0530

    getPostHandler function

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+	"strconv"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/shantanuvidhate/feeds-backend/internal/store"
 )
 
@@ -36,6 +39,31 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	}
 
 	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+}
+func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
+	idParam := chi.URLParam(r, "postId")
+	id, err := strconv.ParseInt(idParam, 10, 64)
+
+	if err != nil {
+		writeJSONError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	ctx := r.Context()
+	post, err := app.store.Post.GetById(ctx, id)
+	if err != nil {
+		switch {
+		case errors.Is(err, store.ErrRecordNotFound):
+			writeJSONError(w, http.StatusNotFound, err.Error())
+		default:
+			writeJSONError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+
+	if err := writeJSON(w, http.StatusOK, post); err != nil {
 		writeJSONError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
