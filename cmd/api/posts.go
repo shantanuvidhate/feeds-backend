33284ef644f4575fc52effbc403fb45fc7cf3986
shantanuvidhate <shantanuvidhate00@gmail.com>
2025-01-18 17:34:23 +0530

    createpost handler

---
+++ cmd/api/posts.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/shantanuvidhate/feeds-backend/internal/store"
+)
+
+type createPostPayload struct {
+	Title   string   `json:"title"`
+	Content string   `json:"content"`
+	Tags    []string `json:"tags"`
+}
+
+func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request) {
+	var payload createPostPayload
+	// Temp user
+
+	if err := readJSON(w, r, &payload); err != nil {
+		writeJSONError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	post := &store.Post{
+		Title:   payload.Title,
+		Content: payload.Content,
+		Tags:    payload.Tags,
+		UserId:  1,
+	}
+
+	ctx := r.Context()
+
+	if err := app.store.Post.Create(ctx, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+}
