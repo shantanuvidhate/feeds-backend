4c704ebabcfe139c0434a2e95e6c57778b3e4703
shantanuvidhate <shantanuvidhate00@gmail.com>
2025-01-18 17:33:03 +0530

    Read JSON helper function

--- cmd/api/json.go
+++ cmd/api/json.go
@@ -10,3 +10,19 @@ func writeJSON(w http.ResponseWriter, status int, data any) error {
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(data)
 }
+
+func readJSON(w http.ResponseWriter, r *http.Request, data any) error {
+	// Block the size of the request body to prevent potential attacks
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+	return decoder.Decode(data)
+}
+
+func writeJSONError(w http.ResponseWriter, status int, message string) error {
+	type envelop struct {
+		Error string `json:"error"`
+	}
+	return writeJSON(w, status, &envelop{Error: message})
+}
