61110e7edcc8d11f7f5c3b7123445c0008a6d347
shantanuvidhate <shantanuvidhate00@gmail.com>
2025-01-25 15:38:15 +0530

    execution for DB seed

---
+++ cmd/migrate/migrations/seed/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+
+	"github.com/shantanuvidhate/feeds-backend/internal/db"
+	"github.com/shantanuvidhate/feeds-backend/internal/env"
+	"github.com/shantanuvidhate/feeds-backend/internal/store"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	addr := env.GetString("DB_ADDR", "postgres://user:password@localhost:5432/feeds?sslmode=disable")
+	conn, err := db.New(addr, 5, 5, "15m")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+
+	store := store.NewStorage(conn)
+
+	db.Seed(&store)
+}
