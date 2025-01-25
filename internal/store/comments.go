62e5b1edd94efab9acb6dde9d368117e3adad3b3
shantanuvidhate <shantanuvidhate00@gmail.com>
2025-01-25 15:34:42 +0530

    create comments ftr

--- internal/store/comments.go
+++ internal/store/comments.go
@@ -24,6 +24,8 @@ func (s *CommentStore) GetByPostId(ctx context.Context, postId int64) ([]Comment
 	WHERE c.post_id = $1
 	ORDER BY c.created_at DESC
 	`
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeOutDuration)
+	defer cancel()
 
 	rows, err := s.db.QueryContext(ctx, query, postId)
 	if err != nil {
@@ -50,4 +52,22 @@ func (s *CommentStore) GetByPostId(ctx context.Context, postId int64) ([]Comment
 		comments = append(comments, comment)
 	}
 	return comments, nil
+}
+
+func (s *CommentStore) Create(ctx context.Context, comment *Comment) error {
+	query := `INSERT INTO comments (post_id, user_id, content) VALUES ($1, $2, $3) RETURNING id, created_at`
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeOutDuration)
+	defer cancel()
+
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		comment.PostId,
+		comment.UserId,
+		comment.Content,
+	).Scan(&comment.ID, &comment.CreatedAt)
+	if err != nil {
+		return err
+	}
+	return nil
 }
