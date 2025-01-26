5efbd8faa81c2f01b95c04fe384f5ce59c6249b1
shantanuvidhate <shantanuvidhate00@gmail.com>
2025-01-26 17:47:31 +0530

    getuserhandler function

---
+++ cmd/api/users.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/shantanuvidhate/feeds-backend/internal/store"
+)
+
+func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
+	userId, err := strconv.ParseInt(chi.URLParam(r, "userId"), 10, 64)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	ctx := r.Context()
+	user, err := app.store.User.GetById(ctx, userId)
+	if err != nil {
+		switch err {
+		case store.ErrRecordNotFound:
+			app.notFoundResponse(w, r, err)
+			return
+		default:
+			app.internalServerError(w, r, err)
+			return
+		}
+
+	}
+
+	if err := app.jsonResponse(w, http.StatusOK, user); err != nil {
+		app.internalServerError(w, r, err)
+	}
+}
