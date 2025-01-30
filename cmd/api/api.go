95e2c8ca84f2dc1a4c4fd10d43be0a855dd23ef8
shantanuvidhate <shantanuvidhate00@gmail.com>
2025-01-30 23:11:03 +0530

    followUserHandler API chi Route

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -58,6 +58,8 @@ func (app *application) mount() http.Handler {
 		r.Route("/users", func(r chi.Router) {
 			r.Route("/{userId}", func(r chi.Router) {
 				r.Get("/", app.getUserHandler)
+				r.Put("/follow", app.followUserHandler)
+				r.Put("/unfollow", app.unfollowUserHandler)
 			})
 		})
 	})
