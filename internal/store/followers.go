e75471f40d49d8e1b14746b4a173627b11d74cbc
shantanuvidhate <shantanuvidhate00@gmail.com>
2025-02-20 00:28:48 +0530

    seperated the follower store from user

---
+++ internal/store/followers.go
@@ -0,0 +1,45 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/lib/pq"
+)
+
+type Followers struct {
+	UserId     int64  `json:"user_id"`
+	FollowerId int64  `json:"follower_id"`
+	CreatedAt  string `json:"created_at"`
+}
+
+type FollowerStore struct {
+	db *sql.DB
+}
+
+func (s *FollowerStore) Follow(ctx context.Context, followerId, userId int64) error {
+	query := `INSERT INTO followers (user_id, follower_id) VALUES ($1, $2)`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeOutDuration)
+	defer cancel()
+
+	_, err := s.db.ExecContext(ctx, query, userId, followerId)
+
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok && pqErr.Code == "23505" {
+			return ErrConflict
+		}
+	}
+	return nil
+}
+
+func (s *FollowerStore) Unfollow(ctx context.Context, followerId, userId int64) error {
+	query := `DELETE FROM followers WHERE user_id = $1 AND follower_id = $2`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeOutDuration)
+	defer cancel()
+
+	_, err := s.db.ExecContext(ctx, query, userId, followerId)
+
+	return err
+}
