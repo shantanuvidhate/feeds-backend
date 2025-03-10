cf8c006cff85f645b18afcd6800ded519060c620
shantanuvidhate <shantanuvidhate00@gmail.com>
2025-03-10 11:48:02 +0530

    swagger params for main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,6 +11,23 @@ import (
 
 const version = "0.0.1"
 
+//	@title			Feed Backend API
+//	@description	API for Feed Generation, a social network for user
+//	@termsOfService	http://swagger.io/terms/
+
+//	@contact.name	API Support
+//	@contact.url	http://www.swagger.io/support
+//	@contact.email	support@swagger.io
+
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @BasePath					/v1
+//
+// @securityDefinitions.apikey	ApiKeyAuth
+// @in							header
+// @name						Authorization
+// @description
 func main() {
 
 	err := godotenv.Load()
@@ -19,7 +36,8 @@ func main() {
 	}
 
 	cfg := config{
-		addr: env.GetString("ADDR", ":8080"),
+		addr:   env.GetString("ADDR", ":8080"),
+		apiURL: env.GetString("EXTERNAL_URL", "localhost:8080"),
 		db: dbConfig{addr: env.GetString("DB_ADDR", "postgres://user:password@localhost:5432/feeds?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
