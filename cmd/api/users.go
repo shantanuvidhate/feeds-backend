dd33d63521f331312fd56a1cc866e73922feeea3
shantanuvidhate <shantanuvidhate00@gmail.com>
2025-03-10 13:41:14 +0530

    API Docs for getUser Endpoint

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -13,6 +13,20 @@ type userKey string
 
 const userCtx userKey = "user"
 
+// GetUser godocs
+//
+//	@Summary		Fetchs a user profile
+//	@Description	Fetchs a user profile by user_id
+//	@Tags			users
+//	@Accept			json
+//	@Produce		json
+//	@Param			userId	path		int	true	"User ID"
+//	@Success		200		{object}	store.User
+//	@Failure		400		{object}	error
+//	@Failure		404		{object}	error
+//	@Failure		500		{object}	error
+//	@Security		ApiKeyAuth
+//	@Router			/users/{userId} [get]
 func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
 	user := getUserFromContext(r)
 
